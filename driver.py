f8050ff736aa8e48853605a709a617a26971b096
EvanFarry <evan.farry3@gmail.com>
2016-12-14 09:18:15 -0600

    Updated driver.py

--- driver.py
+++ driver.py
@@ -1,3 +1,16 @@
 from classes.pokemon import Pokemon
 
-print "Hello World"
+import json
+
+user_input = "1"
+
+p = Pokemon(user_input)
+poke_id = p.get_pokemon_id()
+name = p.get_pokemon_name()
+types =  p.get_pokemon_types()
+
+
+if len(types) == 1:
+    print poke_id, name, types[0]
+elif len(types) == 2:
+    print poke_id, name, types[0], types[1]
